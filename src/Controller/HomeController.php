3d766cc60fccb3bd43468c5ff402734c46f9a077
joelbabay <babayjoel7@gmail.com>
2024-07-10 08:35:06 +0300

    menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
-use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,19 +10,46 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
         $homeArticle = $articleRepository
-            ->find(1);
-            
+            ->find(2);
+
         $articles = $articleRepository->findAll();
-        
-        $categories = $categoryRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'articles' =>$articles,
-            'homeArticle' => $homeArticle,
-            'categories' => $categories
+            'homeArticle' => $homeArticle
         ]);
+    }
+
+    #[Route('/methodes', name: 'app_methode')]
+    public function method(): Response
+    {
+        return $this->render('home/method.html.twig');
+    }
+
+    #[Route('/perdant', name: 'app_perdant')]
+    public function perdant(): Response
+    {
+        return $this->render('home/perdant.html.twig');
+    }
+
+    #[Route('/citation', name: 'app_citation')]
+    public function citation(): Response
+    {
+        return $this->render('home/citation.html.twig');
+    }
+
+    #[Route('/bien-debuter', name: 'app_bien_debuter')]
+    public function bienDebuter(): Response
+    {
+        return $this->render('home/bienDebuter.html.twig');
+    }
+
+    #[Route('/performance', name: 'app_performance')]
+    public function performance(): Response
+    {
+        return $this->render('home/performance.html.twig');
     }
 }
